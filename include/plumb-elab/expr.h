8bf57ddeb3089d385f75159faeea3511dc302d1b
feelingkrummy <kamrummel95@gmail.com>
2024-11-01 09:28:16 -0400

    Some incremental work on expression types

--- include/plumb-elab/expr.h
+++ include/plumb-elab/expr.h
@@ -2,10 +2,21 @@
 #define PLUMB_EXPR_H_
 
 typedef enum {
-    PlumbExpr_Grouping,
-    PlumbExpr_Binary,
-    PlumbExpr_Unary,
-    PlumbExpr_Variable
+    PLUMB_OP_PLUS, 
+    PLUMB_OP_MINUS, 
+    PLUMB_OP_DIV, 
+    PLUMB_OP_MULT, 
+    PLUMB_OP_POWER, 
+
+    PLUMB_TOKEN_EQUAL, 
+    PLUMB_TOKEN_EQUALEQUAL, 
+} PlumbOp;
+
+typedef enum {
+    PLUMB_EXPR_GROUPING,
+    PLUMB_EXPR_BINARY,
+    PLUMB_EXPR_UNARY,
+    PLUMB_EXPR_VARIABLE
 } PlumbExprType;
 
 typedef struct {
